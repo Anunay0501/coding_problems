979dec5d052c7bfc0ca6bdf79e928f4a18f2e664
Anunay0501 <anunay0911sinha@gmail.com>
2023-02-20 17:14:24 +0530

    h

--- LeetCode-103-Binary Tree Zigzag Level Order Traversal.py
+++ LeetCode-103-Binary Tree Zigzag Level Order Traversal.py
@@ -10,28 +10,66 @@ import collections
 
 # From top of the head, we can do a bfs and store each level and store'em. Then reverse the alternate levels.
 
+
 class Solution:
     def zigzagLevelOrder(self, root):
-        ans=[]
+        ans = []
         if not root:
             return ans
-            
-        dic=defaultdict(list)
+
+        dic = defaultdict(list)
+
+        qu = collections.deque([(root, 0)])
         
-        qu=collections.deque([(root,0)])
         while qu:
-            node,depth=qu.popleft()
-            dic[depth].append(node.val)
-            if node.left:
-                qu.append((node.left,depth+1))
-            if node.right:
-                qu.append((node.right,depth+1))
         
+            node, depth = qu.popleft()
+            dic[depth].append(node.val)
+        
+            if node.left:
+                qu.append((node.left, depth+1))
+        
+            if node.right:
+                qu.append((node.right, depth+1))
+
         for key in dic:
-            if key%2==0:
+            if key % 2 == 0:
                 ans.append(dic[key])
             else:
                 ans.append(dic[key][::-1])
+        
         return ans
 
-# Optimising points - a) 
+# Optimising points - a) can be more memory efficient.
+
+
+class Solution:
+    def zigzagLevelOrder(self, root):
+        
+        if not root:
+            return []
+        
+        result = []
+        qu, dir = collections.deque([root]), 1
+        
+        while qu:
+        
+            lvl = []                                    # Saving some memory here by storing just a level during bfs.
+        
+            for _ in range(len(qu)):
+        
+                node = qu.popleft()
+                lvl.append(node.val)
+        
+                if node.left:
+                    qu.append(node.left)
+        
+                if node.right:
+                    qu.append(node.right)
+        
+            result.append(lvl[::dir])
+        
+            dir *= -1
+        
+        
+        return result
