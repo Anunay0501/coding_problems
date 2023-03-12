c20d72a5984619657ba405225cf264726a03ced3
Anunay0501 <anunay0911sinha@gmail.com>
2023-03-12 17:30:59 +0530

    hello

---
+++ Leetcode-2587-Rearrange_Array_to_Maximize_Prefix_Score.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def maxScore(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+        ans,curr=0,0
+        for num in nums:                                # We can alternately use accumulate.
+            ans+=1
+            curr+=num
+            if curr<=0:
+                return ans-1                        
+        return ans
