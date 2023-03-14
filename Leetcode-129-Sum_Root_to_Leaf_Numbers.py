6054c4332425ac068e80955a8b478fffa4f4a608
Anunay0501 <anunay0911sinha@gmail.com>
2023-03-14 19:32:19 +0530

    xyz

--- Leetcode-129-Sum_Root_to_Leaf_Numbers.py
+++ Leetcode-129-Sum_Root_to_Leaf_Numbers.py
@@ -7,32 +7,39 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+
 class Solution:                                             # recursive solution
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        self.ans=0
-        def dfs(node,s):
+        self.ans = 0
+
+        def dfs(node, s):
             if not node.left and not node.right:
-                self.ans+=int(s+str(node.val))
+                self.ans += int(s+str(node.val))
             else:
                 if node.left:
-                    dfs(node.left,s+str(node.val))
+                    dfs(node.left, s+str(node.val))
                 if node.right:
-                    dfs(node.right,s+str(node.val))
-        dfs(root,'')
+                    dfs(node.right, s+str(node.val))
+        
+        dfs(root, '')
+        
         return self.ans
 
 
 class Solution:                                             # iterative solution
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        ans=0
-        stck=collections.deque([(root,str(root.val))])
+        ans = 0
+        stck = collections.deque([(root, str(root.val))])
+        
         while stck:
-            node,s=stck.pop()
+            node, s = stck.pop()
             if not node.left and not node.right:
-                ans+=int(s)
+                ans += int(s)
             else:
                 if node.left:
-                    stck.append((node.left,s+str(node.left.val)))
+                    stck.append((node.left, s+str(node.left.val)))
                 if node.right:
-                    stck.append((node.right,s+str(node.right.val)))
+                    stck.append((node.right, s+str(node.right.val)))
+        
         return ans
